200d18644a0b46ca1d78852586f82f1846d3ab90
amirhosin <amirhosinshahsavan@gmail.com>
2024-06-11 01:36:17 +0330

    editing..

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import  Post, Category,Comment
 
-def home(req):
-    return render(req,'index.html',{})
+def home(request):
+    all_news=Post.objects.all()
+    return render(request,'all-news.html',{
+        'all_news':all_news
+    })
